e2ce9b8748064b6c05e5a725e3931bbffd608fa9
Jonathan Sebastian <jojosebseb@gmail.com>
2017-10-11 14:47:48 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -86,3 +86,114 @@ $('#indexSection-2').waypoint(function() {
     });
     $('#indexSection-2').removeClass('entry');
 },{offset: '70%'});
+
+$('#moduleBelt').waypoint(function() {
+    setTimeout(function(){
+        $('#moduleBelt .flex-module').css({
+            'transition': 'all .3s ease'
+        });
+   }, 2000);
+
+    $('#moduleBelt .flex-module').each(function(i){
+        $(this).css({
+            'transition': 'all .5s ease '+i/10+'s'
+        });
+    });
+    $('#moduleBelt').removeClass('entry');
+
+},{offset: '70%'});
+
+$('#featureBelt').waypoint(function() {
+    setTimeout(function(){
+        $('#featureBelt .flex-module').css({
+            'transition': 'all .3s ease'
+        });
+   }, 2000);
+
+    $('#featureBelt .flex-module').each(function(i){
+        $(this).css({
+            'transition': 'all .5s ease '+i/10+'s'
+        });
+    });
+    $('#featureBelt').removeClass('entry');
+
+},{offset: '70%'});
+
+$('#conceptBelt').waypoint(function() {
+    setTimeout(function(){
+        $('#conceptBelt .flex-module').css({
+            'transition': 'all .3s ease'
+        });
+   }, 2000);
+
+    $('#conceptBelt .flex-module').each(function(i){
+        $(this).css({
+            'transition': 'all .5s ease '+i/10+'s'
+        });
+    });
+    $('#conceptBelt').removeClass('entry');
+
+},{offset: '70%'});
+
+$('#mainBelt').waypoint(function() {
+    $('#mainBelt').removeClass('entry');
+},{offset: '0%'});
+
+$('#supportBanner').waypoint(function() {
+    $('#supportBanner .flex-module').each(function(i){
+        $(this).css({
+            'transition-delay': i/10+'s'
+        })
+    });
+    $('#supportBanner').removeClass('entry');
+},{offset: '70%'});
+
+$('#aboutSection-1').waypoint(function() {
+    $('#aboutSection-1').removeClass('entry');
+},{offset: '70%'});
+
+$('#aboutSection-2').waypoint(function() {
+    $('#aboutSection-2').removeClass('entry');
+},{offset: '70%'});
+
+$('#aboutSection-3').waypoint(function() {
+    $('#aboutSection-3').removeClass('entry');
+},{offset: '70%'});
+
+$('#indexSection-2 .flex').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  // centerMode: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
