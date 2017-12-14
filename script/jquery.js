0760f7272444e1e3cb590bd7d7db8e06eca89e94
Jonathan Sebastian <jojosebseb@gmail.com>
2017-12-14 14:20:16 +0700

    v1

--- script/jquery.js
+++ script/jquery.js
@@ -192,14 +192,14 @@ $('#indexSection-2 .flex').slick({
   speed: 300,
   slidesToShow: 6,
   slidesToScroll: 1,
-  // centerMode: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 4,
+        slidesToScroll: 4,
         infinite: true,
+        // variableWidth: true,
         dots: true
       }
     },
@@ -217,8 +217,5 @@ $('#indexSection-2 .flex').slick({
         slidesToScroll: 2
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
